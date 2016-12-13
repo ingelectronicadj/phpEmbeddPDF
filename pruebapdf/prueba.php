7f0606cb2de0a3eeaafc883fbef06aa5ee9f77c6
ingelectronicadj <ingelectronicadj@gmail.com>
2016-12-12 23:00:28 -0500

    Se arregla fichero base y se añade un logo

--- pruebapdf/prueba.php
+++ pruebapdf/prueba.php
@@ -4,20 +4,17 @@
 			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
 			  crossorigin="anonymous"></script>
 <select id="misarchivos">
-  <option value="tal"></option>
-
-<?php
-$archivos = scandir("/home/asus/Escritorio/www/pdf"); //Se carga la ruta del enlace simbolico
-
-foreach ($archivos as $archivo) {
-   echo "<option value='".$archivo."'>".$archivo. "</option>"; //Concatenamos archivos con ".$archvivo."
-
-} ?>
+	<?php
+		$archivos = scandir("/home/asus/Escritorio/www/pdf"); //Se carga la ruta del enlace simbolico
+		foreach ($archivos as $archivo) {
+		   echo "<option value='".$archivo."'>".$archivo. "</option>"; //Concatenamos archivos con ".$archvivo."
+			}
+	?>
 </select>
+
 <!--Se cargan mis archivos en la variable pdf, se añade el archivo actual al html con el atributo data en el visor-->
 <script>
 $(function(){
-
   $("#misarchivos").on("change",function(){
     var pdf = $("#misarchivos").val();
     $("#archivoactual").html(pdf);
@@ -28,3 +25,4 @@ $(function(){
 <p> Archivo actual </p>
 <center><object style="width:1100px;height:600px" id="visor" data=""></object></center> <!--Se sirven los pdf embebidos -->
 <p id="archivoactual"></p> <!--Carga archivo actual al id de la  -->
+<div align="center"><img src="logo.jpg"></div>
